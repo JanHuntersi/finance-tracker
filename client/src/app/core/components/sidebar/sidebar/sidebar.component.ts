048b81e3a9c48e94be3e71c95a7d3c3a819ad5bd
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-07-27 15:10:47 +0200

    Updated navigation to a sidebar, and added some navigation links to it

---
+++ client/src/app/core/components/sidebar/sidebar/sidebar.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {AuthService} from "../../../services/auth-service.service";
+import {NgClass, NgIf} from "@angular/common";
+import {MatIcon} from "@angular/material/icon";
+import {MatListItem} from "@angular/material/list";
+import {NavigationItemComponent} from "../navigation-item/navigation-item.component";
+
+@Component({
+  selector: 'sidebar',
+  standalone: true,
+  imports: [
+    RouterLink,
+    RouterLinkActive,
+    NgIf,
+    MatIcon,
+    NgClass,
+    MatListItem,
+    NavigationItemComponent
+  ],
+  templateUrl: './sidebar.component.html',
+  styleUrl: './sidebar.component.css'
+})
+export class Sidebar {
+  public isOpen: boolean = true;
+
+  public constructor(
+    public authService: AuthService,
+  ) {}
+
+  public toggleSidebar(): void {
+    this.isOpen = !this.isOpen;
+  }
+}
