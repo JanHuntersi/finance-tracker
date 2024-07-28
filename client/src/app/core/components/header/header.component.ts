0a211d5e2ab727c43b5e3a4d306addf45d14819e
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-07-28 18:33:08 +0200

    Updated header, fixed bug after deleting an element not showing correct action buttons

--- client/src/app/core/components/header/header.component.ts
+++ client/src/app/core/components/header/header.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import {Sidebar} from "../sidebar/sidebar/sidebar.component";
+import {MatIcon} from "@angular/material/icon";
+import {NgIf} from "@angular/common";
+import {SearchBarComponent} from "./search-bar/search-bar.component";
 
 @Component({
   selector: 'app-header',
   standalone: true,
-    imports: [
-        Sidebar
-    ],
+  imports: [
+    Sidebar,
+    MatIcon,
+    NgIf,
+    SearchBarComponent
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
