49c2ddc275cfda1b7f97ab9fbd886d64aab1d85e
kristjansuligoj <kristjan.suligoj@aspiria.com>
2024-07-28 15:23:25 +0200

    Created icon picker component, that allows the user to pick a component based on the category and updated type-select component

--- client/src/app/features/transactions/transaction-type-select/transaction-type-select.component.ts
+++ client/src/app/features/transactions/transaction-type-select/transaction-type-select.component.ts
@@ -1,14 +1,17 @@
 import {Component, Input} from '@angular/core';
 import {NgClass} from "@angular/common";
-import {AbstractControl, FormGroup} from "@angular/forms";
+import {AbstractControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
 import {TypePipe} from "../../../core/pipes/type-pipe.pipe";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-transaction-type-select',
   standalone: true,
   imports: [
     NgClass,
-    TypePipe
+    TypePipe,
+    MatIcon,
+    ReactiveFormsModule
   ],
   templateUrl: './transaction-type-select.component.html',
   styleUrl: './transaction-type-select.component.css'
@@ -17,6 +20,11 @@ export class TransactionTypeSelectComponent {
   @Input() public form: FormGroup = new FormGroup({});
   @Input() public clickable: boolean = true;
 
+  /**
+   * Set the value of the control
+   *
+   * @param { number } type
+   */
   public selectType(type: number): void {
     if (this.clickable) {
       const control: AbstractControl<any, any> | null = this.form.get('type_id');
---
+++ client/src/app/shared/components/icon-picker/icon-picker.component.ts
@@ -0,0 +1,59 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {MatIcon} from "@angular/material/icon";
+import {NgForOf} from "@angular/common";
+import {AbstractControl, FormGroup, FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {TypePipe} from "../../../core/pipes/type-pipe.pipe";
+import {icons} from "../../../core/config/icons";
+
+@Component({
+  selector: 'app-icon-picker',
+  standalone: true,
+  imports: [
+    MatIcon,
+    NgForOf,
+    FormsModule,
+    ReactiveFormsModule,
+    TypePipe,
+  ],
+  templateUrl: './icon-picker.component.html',
+  styleUrl: './icon-picker.component.css'
+})
+export class IconPickerComponent implements OnInit {
+  @Input() public form: FormGroup = new FormGroup({});
+
+  public icons: any = icons;
+  public selectedCategory = this.icons[0];
+  public selectedIcon: string | null = "";
+
+  public ngOnInit() {
+    this.selectedIcon = this.form.get('icon')?.value;
+  }
+
+  public selectCategory(category: any) {
+    this.selectedCategory = category;
+  }
+
+  /**
+   * Returns icons for the selected category
+   *
+   * @param { any } category
+   */
+  public getIconsForCategory(category: any) {
+    return category ? category.icons : [];
+  }
+
+  /**
+   * Set the value of the control
+   *
+   * @param { string } icon
+   */
+  public onIconSelect(icon: string): void {
+    this.selectedIcon = icon;
+
+    const control: AbstractControl<any, any> | null = this.form.get('icon');
+
+    if (control !== null) {
+      control.setValue(this.selectedIcon);
+    }
+  }
+}
